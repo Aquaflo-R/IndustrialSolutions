92198d91daed16b13fc7053fe5c82522ecfa9b00
prasad5898 <prasad01715@gmail.com>
2026-01-19 13:09:39 +0530

    home text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-primary text-2xl font-bold font-primary">Home </h1>
+      <h1 className="text-primary text-2xl font-bold font-primary">Home page</h1>
     </div>
   );
 };
