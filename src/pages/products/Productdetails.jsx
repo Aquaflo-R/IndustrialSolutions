3e861b3bb1962c1428f96ad3168207fe53c604cc
unknown <rohit.decreations748@gmail.com>
2026-01-19 15:15:15 +0530

    productdetails

--- src/pages/products/Productdetails.jsx
+++ src/pages/products/Productdetails.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Productdetails = () => {
-  return <div>Productdetails</div>;
+  return (
+    <div id="main">
+      <div id="keySpecification">
+        <h2>Key Specifications</h2>
+        <div>
+          <h1>Revolutionizing Energy for a Sustainable Tomorrow</h1>
+          <div>
+            <h1>173k</h1>
+            <h2>kWh Energy Generated</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Productdetails;
