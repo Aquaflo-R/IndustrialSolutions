eda69ac3efab76dfb7c312e55bb7816647b4637d
prasad5898 <prasad01715@gmail.com>
2026-01-19 16:20:09 +0530

    created header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="pt-70 pb-20 border">
       <h1 className="text-primary text-2xl font-bold font-primary">Home page</h1>
     </div>
   );
