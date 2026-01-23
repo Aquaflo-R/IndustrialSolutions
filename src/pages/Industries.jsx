5211f01aa7c1f2316cdf112f7bf8bf6e33c75b31
prasad5898 <prasad01715@gmail.com>
2026-01-23 18:07:09 +0530

    home page

--- src/pages/Industries.jsx
+++ src/pages/Industries.jsx
@@ -1,5 +1,5 @@
 import { motion, useScroll, useTransform } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { Car, Hospital, Bolt, Factory, Warehouse, Amphora } from "lucide-react";
 
 const industries = [
