022f10720a23b3f648cabeebf1c4f77e8b30b1b4
unknown <rohit.decreations748@gmail.com>
2026-01-27 12:36:23 +0530

    pushadd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from "react";
 import Home from "./pages/Home";
-import { HashRouter, Route, Routes } from "react-router";
-
 import About from "./pages/About";
-import Products from "./pages/products/Products";
-import Industries from "./pages/Industries";
-import Process from "./pages/Process";
-import Infrastructure from "./pages/Infrastructure";
-import CaseStudies from "./pages/CaseStudies";
-import Contact from "./pages/Contact";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 
 import Productdetails from "./pages/products/Productdetails";
 import ProductCategory from "./pages/products/Productcategory";
 import Allproducts from "./pages/products/Allproducts";
+
+import Industries from "./pages/Industries";
+import Process from "./pages/Process";
+import Infrastructure from "./pages/Infrastructure";
+import CaseStudies from "./pages/CaseStudies";
+import Contact from "./pages/Contact";
 
 /* INDUSTRY DETAIL PAGES */
 import Automotive from "./pages/industries/Automotive";
@@ -25,49 +24,41 @@ import Manufacturing from "./pages/industries/Manufacturing";
 
 function App() {
   return (
-    <>
-      <div>
-        <HashRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/products" element={<Products />} />
-            <Route
-              path="/products/category/:slug"
-              element={<ProductCategory />}
-            />
-            <Route path="/products/all" element={<Allproducts />} />
-            <Route path="/products/:slug" element={<Productdetails />} />
-            <Route path="/industries" element={<Industries />} />
-            <Route path="/process" element={<Process />} />
-            <Route path="/infrastructure" element={<Infrastructure />} />
-            <Route path="/caseStudies" element={<CaseStudies />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/productsdetails" element={<Productdetails />} />
+    <HashRouter>
+      <Header />
 
-            <Route path="/industries/Automotive" element={<Automotive />} />
-            <Route
-              path="/industries/Pharmaceutical"
-              element={<Pharmaceutical />}
-            />
-            <Route
-              path="/industries/EnergyUtilities"
-              element={<EnergyUtilities />}
-            />
-            <Route
-              path="/industries/LogisticsWarehouse"
-              element={<LogisticsWarehouse />}
-            />
-            <Route path="/industries/OilGas" element={<OilGas />} />
-            <Route
-              path="/industries/Manufacturing"
-              element={<Manufacturing />}
-            />
-          </Routes>
-        </HashRouter>
-      </div>
-    </>
+      <Routes>
+        {/* Core Pages */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Products */}
+        <Route path="/products/all" element={<Allproducts />} />
+        <Route path="/products/category/:slug" element={<ProductCategory />} />
+        <Route path="/products/:slug" element={<Productdetails />} />
+
+        {/* Industries */}
+        <Route path="/industries" element={<Industries />} />
+        <Route path="/industries/automotive" element={<Automotive />} />
+        <Route path="/industries/pharmaceutical" element={<Pharmaceutical />} />
+        <Route
+          path="/industries/energy-utilities"
+          element={<EnergyUtilities />}
+        />
+        <Route
+          path="/industries/logistics-warehouse"
+          element={<LogisticsWarehouse />}
+        />
+        <Route path="/industries/oil-gas" element={<OilGas />} />
+        <Route path="/industries/manufacturing" element={<Manufacturing />} />
+
+        {/* Other Pages */}
+        <Route path="/process" element={<Process />} />
+        <Route path="/infrastructure" element={<Infrastructure />} />
+        <Route path="/caseStudies" element={<CaseStudies />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
